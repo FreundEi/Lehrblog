2b3a2c2675bfed0badb891369a27411cf82152d2
vagrant <vagrant@localhost.localdomain>
2020-09-15 05:52:21 +0000

    [Fix] migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_12_101305) do
+ActiveRecord::Schema.define(version: 2020_09_14_142738) do
 
   create_table "article_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 2020_09_12_101305) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inquiries", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
