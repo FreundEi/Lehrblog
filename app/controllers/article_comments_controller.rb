05cf6b60d2181e1123f168591131f8730e6b595e
vagrant <vagrant@localhost.localdomain>
2020-09-23 19:44:08 +0000

    [Fix] before_action

--- app/controllers/article_comments_controller.rb
+++ app/controllers/article_comments_controller.rb
@@ -1,4 +1,6 @@
 class ArticleCommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     article = Article.find(params[:article_id])
     comment = current_user.article_comments.new(article_comment_params)
